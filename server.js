03922ff1226771af75943a96d2933fee69023b71
psitsha <sitsha.polite@yahoo.ie>
2024-01-08 22:57:48 +0000

    installed nodemon as dev dependency

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//routes
+//routes for web browser
+
 app.get('/', (req, res) => {
     res.send('Hello Node API with CRUD')
+})
+
+app.get('/nodemon', (req, res) => {
+    res.send('Hello Node with Nodemon')
 })
 
 
