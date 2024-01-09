efc16d0b8954e02493f125d66662b62b2a811017
psitsha <sitsha.polite@yahoo.ie>
2024-01-09 13:18:00 +0000

    use a route and model to delete data in mongodb

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/products', async(req, res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products);
+        
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message})
@@ -35,6 +36,7 @@ app.get('/products/:id', async(req, res) => {
         const {id} = req.params;
         const product = await Product.findById(id);
         res.status(200).json(product);
+
     } catch(error){
         console.log(error.message);
         res.status(500).json({message: error.message})
@@ -47,7 +49,8 @@ app.post('/products', async(req, res) => {
    // res.send(req.body)
    try {
         const product =  await Product.create(req.body)
-        res.status(200).json(product);    
+        res.status(200).json(product);   
+         
    } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message})
@@ -66,8 +69,24 @@ app.put('/products/:id', async(req, res) => {
         // get latest info from the db
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
+
     } catch (error) {
         console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+})
+
+//delete a product
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `Cannot find product with ID ${id}`})
+        }
+        res.status(200).json(product);
+
+    } catch (error) {
         res.status(500).json({message: error.message});
     }
 })
