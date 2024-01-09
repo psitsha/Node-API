26c20a52f750f7eddd5e1d50d1c4f89e4da57f45
psitsha <sitsha.polite@yahoo.ie>
2024-01-09 10:58:12 +0000

    use a model to post/save data to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product =  require('./models/productModel')
 const app = express()
 const port = 3000
+
+//middleware
+app.use(express.json())
 
 //routes for web browser
 
@@ -12,6 +16,20 @@ app.get('/', (req, res) => {
 app.get('/nodemon', (req, res) => {
     res.send('Hello Node with Nodemon')
     })
+
+// save data into databse
+app.post('/product', async(req, res) => {
+    // console.log(req.body);
+   // res.send(req.body)
+   try {
+        const product =  await Product.create(req.body)
+        res.status(200).json(product);    
+   } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+   }
+})
+
 // mongoose.set("strictQuery", false)
 mongoose.connect('mongodb+srv://psitsha:123456Admin@politenodeapi.wriahvk.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(() => {
