4b4d997172a167fad3a31e6352e31cb6e51e024d
psitsha <sitsha.polite@yahoo.ie>
2024-01-09 12:31:54 +0000

    use a route and model to get/fetch data from  mongodb

--- server.js
+++ server.js
@@ -17,8 +17,31 @@ app.get('/nodemon', (req, res) => {
     res.send('Hello Node with Nodemon')
     })
 
+// fetch ALL data from database
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+// fetch a single product from the db
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
 // save data into databse
-app.post('/product', async(req, res) => {
+app.post('/products', async(req, res) => {
     // console.log(req.body);
    // res.send(req.body)
    try {
